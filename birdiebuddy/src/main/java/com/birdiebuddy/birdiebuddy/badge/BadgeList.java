db2722e9cc6555fe6449a74450eacdf7e93a305d
chae2 <saintcharry22@gmail.com>
2023-10-03 22:37:15 +0900

    commitchange_badgelist
    badge에 필요한 java파일들을 커밋함.

---
+++ birdiebuddy/src/main/java/com/birdiebuddy/birdiebuddy/badge/BadgeList.java
@@ -0,0 +1,19 @@
+package com.birdiebuddy.birdiebuddy.badge;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class BadgeList {
+    @Autowired
+    BadgeService badgeService;
+
+    @GetMapping("/badgelist")
+    public List<Badge> getAllBadges(){
+        List<Badge> badges = badgeService.getAllBadges();
+        return badges;
+    }
+}
---
+++ birdiebuddy/src/main/java/com/birdiebuddy/birdiebuddy/badge/BadgeService.java
@@ -0,0 +1,16 @@
+package com.birdiebuddy.birdiebuddy.badge;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BadgeService {
+    @Autowired
+    BadgeRepository badgeRepository;
+
+    public List<Badge> getAllBadges(){
+        return badgeRepository.findAll();
+    }
+}
