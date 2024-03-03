15f301c10199f0d2366d2e22939d2e119938a527
coldnoodle_kjy <kjy11069@sookmyung.ac.kr>
2024-03-03 21:07:05 +0900

    feat: RecordService 추가

--- birdiebuddy/src/main/java/com/birdiebuddy/birdiebuddy/record/repository/RecordRepository.java
+++ birdiebuddy/src/main/java/com/birdiebuddy/birdiebuddy/record/repository/RecordRepository.java
@@ -3,7 +3,14 @@ package com.birdiebuddy.birdiebuddy.record.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RecordRepository extends JpaRepository<Record,Long> {
+    //모든 record 조회
+    List<Record> findAll();
+
+
+
 
 }
---
+++ birdiebuddy/src/main/java/com/birdiebuddy/birdiebuddy/record/service/RecordService.java
@@ -0,0 +1,22 @@
+package com.birdiebuddy.birdiebuddy.record.service;
+
+import com.birdiebuddy.birdiebuddy.record.repository.RecordRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class RecordService {
+    private final RecordRepository recordRepository;
+
+    //기록 전체 조회
+    public List<Record> findAllRecords(){
+        return recordRepository.findAll();
+    }
+
+    
+}
